8c21f9dcc2e1033ae8bba876e1b2485a65992fe0
DESKTOP-O5NH5C4\Miner <kalanamapalagama@gmail.com>
2021-12-01 14:31:23 +0530

    initial commit

---
+++ src/main/java/com/example/Unigram/UnigramApplication.java
@@ -0,0 +1,13 @@
+package com.example.Unigram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnigramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnigramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Unigram/UnigramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Unigram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnigramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
